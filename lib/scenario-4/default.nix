0ee7324bc7e06fc8b266eeeb415295dec4e3b5f7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-08-15 21:54:54 +0200

    chore: add `sha256` attribute to `fetchTarball`

--- lib/scenario-4/default.nix
+++ lib/scenario-4/default.nix
@@ -1,5 +1,5 @@
 {
-  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7872526e9c5332274ea5932a0c3270d6e4724f3b.tar.gz") { }
+  pkgs ? import (fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/7872526e9c5332274ea5932a0c3270d6e4724f3b.tar.gz"; sha256 = "0ya1nvcjcsadi9vrvjbi2lh6m9fjb759miy1236rkv4xz7j8bgiy"; }) { }
 }:
 
 pkgs.stdenv.mkDerivation {
