77df6a328cad14063f53a9a7db21c372a6bd27a6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-07-08 17:33:12 +0200

    feat: add formatter for bibliography

--- nix/imports/fmt.nix
+++ nix/imports/fmt.nix
@@ -2,47 +2,54 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem = {
-    treefmt = {
-      projectRootFile = "flake.nix";
-      programs = {
-        nixfmt.enable = true;
-        prettier.enable = true;
-        shfmt.enable = true;
-        shellcheck.enable = true;
-        typstyle.enable = true;
-      };
-      settings = {
-        on-unmatched = "fatal";
-        global.excludes = [
-          ".editorconfig"
-          ".prettierrc"
-          "LICENSE"
-          "tests/lib/bashunit"
-          ".prettierignore"
-          ".shellcheckrc"
-          "*.pdf"
-          "*.png"
-          "*.pem"
-          "*.key"
-          ".envrc"
-          ".github/CODEOWNERS"
-          "CITATION.cff"
-          "lib/**"
-          "*.pl"
-          "*.dot"
-          "*.svg"
-          "*.jpeg"
-          "*.jpg"
-          "*.log"
-          "*.c"
-          "resources/sourcecode/example-makefile"
-          "*.dockerfile"
-          "*.bib"
-          "nix/pkgs/passive/passive.sh"
-          "nix/pkgs/weasel/weasel.sh"
-        ];
+  perSystem =
+    { pkgs, lib, ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs = {
+          nixfmt.enable = true;
+          prettier.enable = true;
+          shfmt.enable = true;
+          shellcheck.enable = true;
+          typstyle.enable = true;
+        };
+        settings = {
+          formatter = {
+            bibtex-tidy = {
+              command = "${lib.getExe pkgs.bibtex-tidy}";
+              includes = [ "*.bib" ];
+            };
+          };
+          on-unmatched = "fatal";
+          global.excludes = [
+            ".editorconfig"
+            ".prettierrc"
+            "LICENSE"
+            "tests/lib/bashunit"
+            ".prettierignore"
+            ".shellcheckrc"
+            "*.pdf"
+            "*.png"
+            "*.pem"
+            "*.key"
+            ".envrc"
+            ".github/CODEOWNERS"
+            "CITATION.cff"
+            "lib/**"
+            "*.pl"
+            "*.dot"
+            "*.svg"
+            "*.jpeg"
+            "*.jpg"
+            "*.log"
+            "*.c"
+            "resources/sourcecode/example-makefile"
+            "*.dockerfile"
+            "nix/pkgs/passive/passive.sh"
+            "nix/pkgs/weasel/weasel.sh"
+          ];
+        };
       };
     };
-  };
 }
